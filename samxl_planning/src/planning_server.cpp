e9e4b20a41e3c0a304c9a11abc1bfea15911c9b6
Michael Ripperger <michael.ripperger@swri.org>
2024-10-29 09:40:37 -0500

    Updated to make the tesseract environment publish TF and visualize with robot model display in Rviz

--- samxl_planning/src/planning_server.cpp
+++ samxl_planning/src/planning_server.cpp
@@ -1,5 +1,9 @@
 #include <rclcpp/node.hpp>
 
+// For the robot_description publisher
+#include <rclcpp/publisher.hpp>
+#include <std_msgs/msg/string.hpp>
+
 #include <tesseract_environment/environment.h>
 #include <tesseract_monitoring/environment_monitor.h>
 #include <tesseract_rosutils/plotting.h>
@@ -26,9 +30,17 @@ public:
     // Create and configure the environment monitor (i.e., connection to the ROS ecosystem)
     std::shared_ptr<rclcpp::Node> node_ptr(this, [](rclcpp::Node*){});
 
+    // Create a publisher for the robot description (for the joint state publisher, etc.)
+    urdf_pub_ = create_publisher<std_msgs::msg::String>("robot_description", rclcpp::QoS(1).reliable().transient_local());
+    {
+      std_msgs::msg::String msg;
+      msg.data = urdf;
+      urdf_pub_->publish(msg);
+    }
+
     monitor_ = std::make_shared<tesseract_monitoring::ROSEnvironmentMonitor>(node_ptr, env_, "samxl");
-    monitor_->startStateMonitor("/joint_states", false);
-    monitor_->setEnvironmentPublishingFrequency(10.0);
+    monitor_->startStateMonitor("/joint_states", true);
+    monitor_->setEnvironmentPublishingFrequency(30.0);
     monitor_->startPublishingEnvironment();
 
     // Create and configure the plotter to visualize the environment (and other things)
@@ -39,6 +51,9 @@ protected:
   tesseract_environment::Environment::Ptr env_;
   tesseract_monitoring::ROSEnvironmentMonitor::Ptr monitor_;
   tesseract_rosutils::ROSPlotting::Ptr plotter_;
+
+  // ROS interfaces
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr urdf_pub_;
 };
 
 int main(int argc, char** argv)
